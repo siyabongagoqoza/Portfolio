38c099da67035efb47ea35b271df4779fd141011
siyabongagoqoza <66440094+siyabongagoqoza@users.noreply.github.com>
2022-01-28 10:13:07 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import  React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App" >
-      <Router>
+      <BrowserRouter>
          <Navbar />
-         <Switch>
+         <Routes>
            <Route path='/' exact />    
-         </Switch>
-      </Router>     
+         </Routes>
+      </BrowserRouter>     
     </div>
   );
 }
