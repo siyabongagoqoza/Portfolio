2b2dc4e1613a27c9af949ec64ec4e5c934431a5e
siyabongagoqoza <66440094+siyabongagoqoza@users.noreply.github.com>
2022-02-03 09:57:43 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,41 +10,53 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 
-const styles = {
-  paperContainer: {
-    backgroundImage: `url(${Image})`,
-    // height: window.innerHeight + 'px'
-  }
-}
-
-
 function App() {
   AOS.init();
   return (
-    <Paper style={styles.paperContainer}>
-        <div className="App" >
-          <meta name="viewport" content="initial-scale=1, width=device-width" />  
-          <BrowserRouter>
-            <ColorTabs />
-            <Routes>
-              <Route path='/' exact />    
-            </Routes>
-          </BrowserRouter>     
-        </div>
-        <div className='body'>
-            <div id='aboutnav'></div>
-            <h1 className='title animate__animated animate__fadeInLeft' data-aos="fade-right">Game Developer and Software Engineer</h1>
-            <div className='imagedescription'>
-              <p className='aboutme' data-aos="fade-right">
-              For 2 years i have worked and upskilled myself as a junior software developer and as a digital assistance for two non profit organizations in an internship to have the approppriate skills to bring a client's vision to life.</p>
-              <div id="cubediv">
-                <img src={CubeImg} className='cubeimg' alt=''/>
+    <div className="portfolio">
+      <div className='homepage' > 
+          <div className="App" >
+            <meta name="viewport" content="initial-scale=1, width=device-width" />  
+            <BrowserRouter>
+              <ColorTabs />
+              <Routes>
+                <Route path='/' exact />    
+              </Routes>
+            </BrowserRouter>     
+          </div>
+          <div className='body'>
+              <div id='aboutnav'></div>
+              <h1 className='title animate__animated animate__fadeInLeft' data-aos="fade-right">Game Developer and Software Engineer</h1>
+              <div className='imagedescription'>
+                <p className='aboutme' data-aos="fade-right">
+                For 2 years i have worked and upskilled myself as a junior software developer and as a digital assistance for two non profit organizations in an internship to have the approppriate skills to bring a client's vision to life.</p>
+                <div id="cubediv">
+                  <img src={CubeImg} className='cubeimg' alt=''/>
+                </div>
+
               </div>
-
-            </div>
-        </div>
-    </Paper>
-
+             
+          </div>
+          
+      </div>
+      <div id='projects'>
+              <h1>Hello there piece of ....</h1>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
+              <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>  
+      </div>      
+  </div>
   );
 }
 
