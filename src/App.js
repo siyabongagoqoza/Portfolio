408bd188793db8f601f63b4522bca46f37186d8c
siyabongagoqoza <66440094+siyabongagoqoza@users.noreply.github.com>
2022-02-03 16:07:13 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import  React from 'react';
 import './App.css';
 import ColorTabs from './components/colourTabs';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { Paper } from '@mui/material';
-import Image from './images/unsplashbluebackground.jpg';
 import CubeImg from './images/cubetech.png';
 import 'animate.css';
 import AOS from 'aos';
@@ -11,27 +9,31 @@ import 'aos/dist/aos.css';
 
 
 function App() {
-  AOS.init();
+  AOS.init({
+    mirror: true,
+  });
   return (
     <div className="portfolio">
       <div className='homepage' > 
           <div className="App" >
             <meta name="viewport" content="initial-scale=1, width=device-width" />  
             <BrowserRouter>
-              <ColorTabs />
               <Routes>
                 <Route path='/' exact />    
               </Routes>
             </BrowserRouter>     
           </div>
+          <div className='navbar'>
+            <ColorTabs/>
+          </div>
           <div className='body'>
               <div id='aboutnav'></div>
-              <h1 className='title animate__animated animate__fadeInLeft' data-aos="fade-right">Game Developer and Software Engineer</h1>
+              <h1 className='title' data-aos="fade-right">Game Developer and Software Engineer</h1>
               <div className='imagedescription'>
                 <p className='aboutme' data-aos="fade-right">
                 For 2 years i have worked and upskilled myself as a junior software developer and as a digital assistance for two non profit organizations in an internship to have the approppriate skills to bring a client's vision to life.</p>
                 <div id="cubediv">
-                  <img src={CubeImg} className='cubeimg' alt=''/>
+                  <a href='#projects'><img src={CubeImg} className='cubeimg' alt=''/></a>
                 </div>
 
               </div>
@@ -40,7 +42,7 @@ function App() {
           
       </div>
       <div id='projects'>
-              <h1>Hello there piece of ....</h1>
+              <h1 data-aos="fade-right">Hello there piece of sheet </h1>
               <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
               <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
               <p>Lorem ipsumszjgesalfhds  sfahdsflkajsgdsfasdjgdsgalkj ajklgadjfdskj alkdsjhdskfadfkjfal ldfhak</p>
